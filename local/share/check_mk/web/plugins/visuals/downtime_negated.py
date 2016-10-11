e0f27b8dce817890195d3e6ffee944746c40a211
makanakoeln <makanakoeln@gmail.com>
2016-10-11 20:38:35 +0200

    Update downtime_negated.py

--- local/share/check_mk/web/plugins/visuals/downtime_negated.py
+++ local/share/check_mk/web/plugins/visuals/downtime_negated.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python# -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-Downtime Comment Negateddeclare_filter(254, FilterText("downtime_comment_negate", _("Downtime comment (negate)"), "downtime", "downtime_comment", "downtime_comment", "!~"))
+declare_filter(254, FilterText("downtime_comment_negate", _("Downtime comment (negate)"), "downtime", "downtime_comment", "downtime_comment", "!~"))
